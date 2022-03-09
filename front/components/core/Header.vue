54b637795f37f5109cded60610bc1d42ca8412d2
francisco.leblanc.sacha <francisco.leblanc.sacha@gmail.com>
2022-03-09 11:53:03 +0100

    v1 Header Front

---
+++ front/components/core/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-black text-white header px-5">
+    <div class="flex justify-between">
+      <div>
+        <h1 class="uppercase titleFont">Wired Beauty</h1>
+      </div>
+      <div class="space-x-8 text-sm my-auto">
+        <NuxtLink to="/test">NEW NOMADIC LABGRADE DEVICE& APP</NuxtLink>
+        <NuxtLink to="/test">WHO WE ARE</NuxtLink>
+        <NuxtLink to="/test">SCIENTIFIC VALIDATION</NuxtLink>
+        <NuxtLink to="/test">STUDIES and DEVICES</NuxtLink>
+      </div>
+      <div class="space-x-6 uppercase text-xs mr-0">
+        <NuxtLink to="/test">Contact us</NuxtLink>
+        <NuxtLink to="/test">Log in</NuxtLink>
+        <NuxtLink to="/test">Become a panelist</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtHeader',
+}
+</script>
+<style>
+.titleFont {
+  font-family: 'Noto Serif';
+  font-size: 38px;
+}
+.header {
+}
+</style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,5 +1,30 @@
+
 <template>
-  <Tutorial />
+  <body>
+    <CoreHeader />
+    <div class="flex">
+      <div style="border: solid black 2px; height: 250px" class="w-3/12 flex">
+        <p class="self-center">
+          DIGITALLY ENABLED LAB & CONSUMER SKINCARE TESTING
+        </p>
+      </div>
+      <div style="border: solid black 2px; height: 250px" class="w-3/6 grid content-between">
+        <p>
+          OUR WEBINAR ON ANTIOXYDATIVES – Register here ! TRUST PILOT – Best app
+          for skin care research.
+        </p>
+        <p>
+          NEW ! DIFFERENT ! BETTER ways to research skincare Hybrid clinical
+          trials and consumer studies
+        </p>
+      </div>
+      <div style="border: solid black 2px; height: 250px" class="w-3/12 flex">
+        <p class="self-center">
+          BIOLOGY + IOT NOMADIC LAB + CONSUMER APP + INSANELY FAST ALGORYTHM
+        </p>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script>
