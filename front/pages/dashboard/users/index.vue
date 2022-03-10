076a9015004020ec31c8dd5de9105781ef423995
horek <azenoxe@gmail.com>
2022-03-10 14:48:36 +0100

    [fix] style of actions in list of users

--- front/pages/dashboard/users/index.vue
+++ front/pages/dashboard/users/index.vue
@@ -31,11 +31,11 @@
                   <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ user.firstname + ' ' + user.lastname }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ user.email }}</td>
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ roles[user.type] }}</td>
-                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                    <NuxtLink :to="`/dashboard/users/${user._id}`" class="text-indigo-600 hover:text-indigo-900">
+                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right sm:pr-6">
+                    <NuxtLink :to="`/dashboard/users/${user._id}`" class="text-sm font-medium text-indigo-600 hover:text-indigo-900">
                       Edit
                     </NuxtLink>
-                    <button type="button" class="text-red-600 hover:text-red-900" @click="deleteUser(user._id)">
+                    <button type="button" class="ml-2 text-sm font-medium text-red-600 hover:text-red-900" @click="deleteUser(user._id)">
                       Delete
                     </button>
                   </td>
